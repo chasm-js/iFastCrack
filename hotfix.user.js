b8d6129cc7cbb3582d7e3ef0e189a3a62c50a0ec
chasm-js <consul-canary3g@icloud.com>
2025-04-08 18:40:13 +0900

    Create hotfix.user.js

---
+++ hotfix.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         iFastCrack HotFix
+// @namespace    chasm-js
+// @version      2025-04-08.v002-fix
+// @description  hotfix for ios FastCrack
+// @author       chasm-js
+// @match        https://crack.wrtn.ai/*
+// @require      https://cdn.jsdelivr.net/gh/chasm-js/iFastCrack/hotfix.js
+// @downloadURL  https://raw.githubusercontent.com/chasm-js/iFastCrack/refs/heads/main/hotfix.user.js
+// @updateURL    https://raw.githubusercontent.com/chasm-js/iFastCrack/refs/heads/main/hotfix.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
